2cabfc9dd49e58af1c86f4d552e20c180fb8aed3
mad0 <madowaty@gmail.com>
2018-05-18 11:18:40 +0200

    Delete some jpgs

--- Survival/game.cpp
+++ Survival/game.cpp
@@ -4,19 +4,26 @@
 Game::Game(Engine* gra) {
 	coll = false;
 	inv = false;
+	fight = false;
 	engine = gra;
 	Wsize = sf::Vector2f(engine->window.getSize());
 	map = std::make_unique<Map>(engine->window, "gfx/maps.gif", Wsize);
 	LoadMap();
-	p1 = std::make_unique<Character>(engine->window, 128);
+	p1 = new Character("gfx/player.png", 128);
 	p1->setPosition(6 * 32, 2 * 32);
-	bag = new Inventory(5);
 	weapon = new Weapons(10, Items::WEAPON, "gfx/dagger.png", "Simple iron dagger", 6, 10);
 	p1->equipWeapon(weapon);
-	bag->addItem(new  Consumable(1, Items::POTION, "gfx/potion.png", "Simple potion", 5, 10));
-	bag->addItem(new  Consumable(1, Items::POTION, "gfx/potion.png", "Simple potion", 5, 10));
-	bag->addItem(new  Consumable(1, Items::POTION, "gfx/potion.png", "Simple potion", 5, 10));
-	bag->addItem(new  Consumable(2, Items::FOOD, "gfx/banana.png", "Banana", 4, 8));
+	enemy.push_back(new Character("gfx/m1.png", 200));
+	enemy[0]->setPosition(4 * 32, 3 * 32);
+	enemy.push_back(new Character("gfx/m2.png", 200));
+	enemy[1]->setPosition(3 * 32, 3 * 32);
+	enemy.push_back(new Character("gfx/m2.png", 200));
+	enemy[2]->setPosition(3 * 32, 4 * 32);
+	//bag = new Inventory(5);
+	//bag->addItem(new  Consumable(1, Items::POTION, "gfx/potion.png", "Simple potion", 5, 10));
+	//bag->addItem(new  Consumable(1, Items::POTION, "gfx/potion.png", "Simple potion", 5, 10));
+	//bag->addItem(new  Consumable(1, Items::POTION, "gfx/potion.png", "Simple potion", 5, 10));
+	//bag->addItem(new  Consumable(2, Items::FOOD, "gfx/banana.png", "Banana", 4, 8));
 	//bag->addItem(new  Consumable(2, Items::FOOD, "gfx/banana.png", "Banana", 4, 8));
 	font.loadFromFile("fonts/Vecna.otf");
 	guiStr[0] = "Health points ";
@@ -30,10 +37,10 @@ Game::Game(Engine* gra) {
 	gui[0].setString(guiStr[0] + "\n      " + std::to_string(p1->getmaxHp()));
 	gui[0].setPosition(50, Wsize.y-100);
 	//Nazwa broni
-	//gui[1].setString(guiStr[1] + weapon->getName());
+	gui[1].setString(guiStr[1] + weapon->getName());
 	gui[1].setPosition(gui[0].getGlobalBounds().width + 100, Wsize.y - 100);
 	//Obrazenia
-	//gui[2].setString(guiStr[2] + std::to_string(weapon->getLdmg()) + "-" + std::to_string(weapon->getHdmg()));
+	gui[2].setString(guiStr[2] + std::to_string(weapon->getLdmg()) + "-" + std::to_string(weapon->getHdmg()));
 	gui[2].setPosition(gui[0].getGlobalBounds().width + 100, Wsize.y - 75);
 	std::cout << "Wchodze z MENU do GRY\n";
 }
@@ -94,18 +101,8 @@ void Game::movePlayer(Directions direct) {
 			std::cout << "RIGHT\n";
 			playerDirection(32, 0);
 			break;
-	}
-	/*
-	if (map->collision(int(nextPos.x/32),  int(nextPos.y/32), p1->getBounds())) {
-		
-		std::cout << "PLAYER: "<<p1->getBounds().left << " " << p1->getBounds().top << "\n";
-		std::cout << "KOLIZJA!\n";
-		//p1->setPosition(actPos.x-x, actPos.y);
-		//p1->getPosition().x / 32 << " " << p1->getPosition().y / 32
-		//std::cout << p1->getPosition().x << " " << p1->getPosition().y << "\n";
-		*/
-
-	}
+	}	
+}
 
 void Game::playerDirection(int x, int y) {
 	sf::Vector2f actPos(p1->getPosition().x/32, p1->getPosition().y/32);
@@ -117,40 +114,56 @@ void Game::playerDirection(int x, int y) {
 	if (map->Interaction(nextPos.x, nextPos.y)) {
 		std::cout << "INTERAKCJA!\n";
 		std::cout << p1->getPosition().x / 32 << " : " << p1->getPosition().y / 32 << "\n";
-	}
-		
-		
-}
-
-void Game::draw() {
-	engine->window.clear();
-	for (auto x: gui)
-		engine->window.draw(x);
-	p1->getWeapon()->itemIconDraw(engine->window);
-	map->draw();
-	p1->draw();
-	if (inv)
-		bag->drawInventory(engine->window);
+	}	
 }
 
 void Game::update() {
 	gui[0].setString(guiStr[0] + "\n      " + std::to_string(p1->getHp()));
 	gui[1].setString(guiStr[1] + p1->getWeapon()->getName());
 	gui[2].setString(guiStr[2] + std::to_string(p1->getWeapon()->getLdmg()) + "-" + std::to_string(p1->getWeapon()->getHdmg()));
+	p1->getWeapon()->itemIcon(sf::Vector2f(Wsize.x / 2 - 64, Wsize.y - 150));
+	fighto();
+
 	//sf::Vector2f kafel(p1->getPosition().x , p1->getPosition().y);
 	//std::cout << "Aktualny kafel: " << int(kafel.x/32) << " " << int(kafel.y/32) << "\n";
 	//coll = map->collision(*p1);
 	//std::cout << p1->getPosition().x << " " << p1->getPosition().y  << "\n";
 	//std::cout << kafel.x + (p1->getBounds().height / 2) + 1<<"\n";
-	p1->getWeapon()->itemIcon(sf::Vector2f(Wsize.x / 2 - 64, Wsize.y - 150));
+	
+}
+void Game::draw() {
+	engine->window.clear();
+	map->draw();
+	for (auto x: gui)
+		engine->window.draw(x);
+	p1->getWeapon()->itemIconDraw(engine->window);
+	for (auto& e:enemy) {
+		if (e->isAlive())
+			e->draw(engine->window);
+	}
+
+	p1->draw(engine->window);
+	if (inv)
+		bag->drawInventory(engine->window);
 }
 
-Game::~Game() {
-	std::cout << "Wychodze z GRY do MENU\n";
-	delete bag;
-	delete weapon;
+void Game::fighto() {
+	if (enemy.size() > 0) {
+		for (int x=0;x<enemy.size();x++) {
+			if (p1->collisionBox().intersects(enemy[x]->collisionBox())) {
+				std::cout << x << "ZNALAZLEM\n";
+				if (enemy[x]->isAlive()) {
+					Fight fight1(p1, enemy[x]);
+				}
+				if (!enemy[x]->isAlive()) {
+					delete enemy[x];
+					enemy.erase(enemy.begin() + x);
+					std::cout << "ENEMy " << enemy.size() << "\n";
+				}
+			}
+		}
+	}
 }
-
 
 void Game::LoadMap() {
 	std::vector<int> Load = {
@@ -199,4 +212,11 @@ void Game::LoadMap() {
 	};
 	map->LoadTile(LoadV);
 	//map->drawMap();
+}
+
+Game::~Game() {
+	std::cout << "Wychodze z GRY do MENU\n";
+	delete bag;
+	delete weapon;
+	delete p1;
 }
